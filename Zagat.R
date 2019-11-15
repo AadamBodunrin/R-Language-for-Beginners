937bafc2a92efa0ca314e92235a9f7a03836fa93
AadamBodunrin <aadamolalekan@gmail.com>
2019-11-15 21:59:08 +0100

    Basic

--- Zagat.R
+++ Zagat.R
@@ -50,5 +50,32 @@ hist(zagat$Price)
 #Show the coulmn price as a histogram indicating its max and min values
 hist(zagat$Price,breaks=seq(8,80,by = 1))
 
+#Show the coulmn price as a histogram indicating its max and min values with "Price" as its title
 hist(zagat$Price, breaks=seq(8,80,by = 1), main="Price")
 axis(1,at=seq(8,80,1))
+
+#Find the percent of price that falls within 1 standard deviation
+U1=lm(m_price+sd_price)
+D1=lm(m_price-sd_price)
+SD1 = U1-D1
+print(SD1)
+
+#Find the percent of price that falls within 2 standard deviation
+U2=lm(m_price+2*sd_price)
+D2=lm(m_price-2*sd_price)
+SD2 = U2-D2
+print(SD2)
+
+#Find the percent of price that falls within 3 standard deviation
+U3=lm(m_price+3*sd_price)
+D3=lm(m_price-3*sd_price)
+SD3 = U3-D3
+print(SD3)
+
+
+#Find the percent of restaurant that have "price" less than or equal to 40
+f <- length(subset(zagat$Name, zagat$Price >= 40))
+percent <- (f/300)*100
+print(percent)
+
+

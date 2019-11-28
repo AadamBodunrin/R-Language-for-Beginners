bb0f0e57f3074a7c3566e92e245e65632fc4d17e
AadamBodunrin <aadamolalekan@gmail.com>
2019-11-28 22:56:16 +0100

    Codes

--- R_Newbies.R
+++ R_Newbies.R
@@ -1,22 +1,54 @@
+#Naming a string using =
 d = "What's good?"
+
+#Requesting for user input
 username <- readline("What's your preferred username? ")
-print(paste(username,',' , d))
+
+#Concatenate username, and "d". Print the output
+print(paste(username,",", d))
+
+#view the first few rows of the iris dataset
 print(head(iris))
+
+#print the dimension of the iris dataset i.e. row V column
 print(dim(iris))
+
+#print all the numbers from 1 to 25 using. 
 print(seq_len(25))
-(x <- 5)
+
+#Create a series of numbers and call out the first item in the series
 shade <- c(10,20,30)
 print(shade[1])
+
+#Create a list of three items
 example_list <- list(a = 10, b = 20, c = 30)
-print (example_list[3])
-print(example_list['b'])
+
+print (example_list[3]) #Print the value of the third item
+
+#Print the value of the item stored as b
+print(example_list["b"]) 
 print(example_list$b)
+
+#create a series of number from 1 to 6
 x <- 1:6
+
+#Print the sequence of the cummaltive addition of numbers in x
 z <- cumsum(x)
 z
 
-nchar('The boy')
-nchar(c('the', 'boy', 'goes'))
+#Print the sqaure root of all the numbers in x
+y <- sqrt(x)
+y
+
+#Print the result of x divided by 2
+a <- x/2
+a
+
+#Find the length of characters in a string
+nchar("The boy")
+
+#Find the length of characters in series of string
+nchar(c("the", "boy", "goes"))
 
 
 
--- Zagat.R
+++ Zagat.R
@@ -41,8 +41,8 @@ sd_price = sd(zagat$Price)
 print(sd_price)
 
 #plot the empirical distribution of price
-lm = ecdf(zagat$Price)
-plot(lm)
+ec = ecdf(zagat$Price)
+plot(ec)
 
 #Show the column price as a histogram
 hist(zagat$Price)
@@ -55,20 +55,20 @@ hist(zagat$Price, breaks=seq(8,80,by = 1), main="Price")
 axis(1,at=seq(8,80,1))
 
 #Find the percent of price that falls within 1 standard deviation
-U1=lm(m_price+sd_price)
-D1=lm(m_price-sd_price)
+U1=ec(m_price+sd_price)
+D1=ec(m_price-sd_price)
 SD1 = U1-D1
 print(SD1)
 
 #Find the percent of price that falls within 2 standard deviation
-U2=lm(m_price+2*sd_price)
-D2=lm(m_price-2*sd_price)
+U2=ec(m_price+2*sd_price)
+D2=ec(m_price-2*sd_price)
 SD2 = U2-D2
 print(SD2)
 
 #Find the percent of price that falls within 3 standard deviation
-U3=lm(m_price+3*sd_price)
-D3=lm(m_price-3*sd_price)
+U3=ec(m_price+3*sd_price)
+D3=ec(m_price-3*sd_price)
 SD3 = U3-D3
 print(SD3)
 
@@ -78,4 +78,19 @@ f <- length(subset(zagat$Name, zagat$Price >= 40))
 percent <- (f/300)*100
 print(percent)
 
+#Let's take a look at Multiple regression
 
+#1. Import the zagat file. This action is the first code in this file.
+
+#2. Use the Multiple regression command and iew the summary of the command
+zagatmod= lm(Price~ Food +Decor+Service, data = zagat)
+print(summary(zagatmod))
+
+#3. Using the leaps library, find the r2 of the predictors to pick the best predictors for the model
+library(leaps)
+leaps= regsubsets(Price~ Food+Decor+Service, data=zagat, nbest=3)
+plot(leaps, scale="r2")
+
+#4. Find the correlation of the numeric variables in the dataset
+y <- cor(zagat[,unlist(lapply(zagat, is.numeric))])
+print(y)
